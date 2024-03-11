58a74a01132f8bec407784cca9de732a42ca73fd
Vitan <vitan.me@gmail.com>
2024-03-11 16:33:28 +0000

    📦 NEW: update package ivitan/ivitan.github.io@a4eae8e08f8696418d18348d7defc88a278a7a69

---
+++ js/services/mdrender.js
@@ -0,0 +1,1 @@
+utils.jq(()=>{for(var e=document.getElementsByClassName("ds-mdrender"),t=0;t<e.length;t++){const s=e[t];var r=s.getAttribute("src")+"?t="+(new Date).getTime();utils.request(s,r,function(e){s.innerHTML=marked.parse(resp.data)})}});
